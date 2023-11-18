9417f0144b5a438ac569ceec6fbfca61b73b0acb
sergio.fontecha <sergiofontecha@MacBook-Pro-de-ApiumHub-2.local>
2023-11-18 18:32:52 +0100

    Improvement: Add styles and fonts. Also adding new sections

--- range_project/app/layout.tsx
+++ range_project/app/layout.tsx
@@ -1,7 +1,13 @@
+import './global.css'
+import { montserrat } from '../assets/fonts'
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${montserrat.className} antialiased`}>
+        {children}
+        <footer className="py-10 flex justify-center items-center">This footer was made with ❤️ by Sergio</footer>
+      </body>
     </html>
   )
 }
