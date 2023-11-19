cac1c01bd67ff0e75a2c7845ee2f258654967593
sergio.fontecha <sergiofontecha@MacBook-Pro-de-ApiumHub-2.local>
2023-11-19 11:04:00 +0100

    Feat: Using sidebar componenet and adapting styles

--- range_project/app/layout.tsx
+++ range_project/app/layout.tsx
@@ -1,12 +1,17 @@
 import './global.css'
 import { montserrat } from '../assets/fonts'
+import SideNav from './ui/home/navComponent'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={`${montserrat.className} antialiased`}>
-        {children}
-        <footer className="py-10 flex justify-center items-center">This footer was made with ❤️ by Sergio</footer>
+    <html lang="en" className="h-screen">
+      <body className={`${montserrat.className} antialiased min-h-screen flex-col`}>
+        <div className="flex min-h-screen">
+          <div className="w-full flex-none md:w-64 bg-gray-300">
+            <SideNav />
+          </div>
+          <div className="flex-grow p-6 md:overflow-y-auto pt-96">{children}</div>
+        </div>
       </body>
     </html>
   )
--- range_project/app/page.tsx
+++ range_project/app/page.tsx
@@ -2,10 +2,10 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="flex justify-center">
-      <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-        <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-          <strong>Welcome to range project</strong>
+    <main className="flex justify-center items-center">
+      <div className="min-w-1/2 flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+        <p className={`text-xl text-center text-gray-800 md:text-3xl md:leading-normal`}>
+          <strong>Welcome to Range Project</strong>
         </p>
       </div>
     </main>
