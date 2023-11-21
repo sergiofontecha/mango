b4203d1cd0504cae3eb3693a22bcdd61577233c1
sergio.fontecha <sergiofontecha@MacBook-Pro-de-ApiumHub-2.local>
2023-11-21 16:55:00 +0100

    Feat: Create service for exercise2

--- range_project/app/share/initialValuesService.ts
+++ range_project/app/share/initialValuesService.ts
@@ -1,16 +1,28 @@
-import { NormalInitialValues } from './interfaces'
+import { RangeValues } from './interfaces'
+
+const requestOptions: RequestInit = {
+  method: 'GET',
+  headers: { 'content-type': 'application/json' },
+}
 
 export async function GetNormalValues() {
-  const requestOptions: RequestInit = {
-    method: 'GET',
-    headers: { 'content-type': 'application/json' },
-  }
-
   try {
     const response = await fetch('http://localhost:3000/api/normalValues', requestOptions)
     if (response.ok) {
       const initialValues = await response.json()
-      return initialValues as unknown as NormalInitialValues
+      return initialValues as unknown as RangeValues
+    }
+  } catch (error) {
+    throw new Error('Failed to fetch data.')
+  }
+}
+
+export async function GetFixedValues() {
+  try {
+    const response = await fetch('http://localhost:3000/api/fixedValues', requestOptions)
+    if (response.ok) {
+      const initialValues = await response.json()
+      return initialValues as unknown as RangeValues
     }
   } catch (error) {
     throw new Error('Failed to fetch data.')
