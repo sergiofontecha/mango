39c48c12043b803cb9c386c7cf3196dbb909ad88
sergio.fontecha <sergiofontecha@MacBook-Pro-de-ApiumHub-2.local>
2023-11-22 13:07:39 +0100

    improvement: Refactot to improve file size and readability

--- range_project/app/ui/rangeSlider/rangeSlider.tsx
+++ range_project/app/ui/rangeSlider/rangeSlider.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useRef, useEffect } from 'react'
-import { setBulletsToStep, setBulletPosition, getNewMinValue, getNewMaxValue } from '@/app/share/utils'
+import { chooseNextStep, setBulletPosition, getNewMinValue, getNewMaxValue } from '@/app/share/utils'
 import { RangeValues } from '@/app/share/interfaces'
 import { InputType, RangeType } from '@/app/share/enums'
 
@@ -12,169 +12,116 @@ export default function RangeSlider({
   initialValues: RangeValues
   rangeType: RangeType
 }) {
-  const minValue = initialValues.values[0]
-  const maxValue = initialValues.values[initialValues.values.length - 1]
-  const [position, setPosition] = useState({ min: minValue, max: maxValue })
-  const [values, setValues] = useState<any>({ min: minValue, max: maxValue })
-  const [tempValues, setTempValues] = useState<{ [key: string]: number | null }>({})
-  const [isDragging, setIsDragging] = useState<boolean>(false)
-  const [dragType, setDragType] = useState<InputType.MIN | InputType.MAX | null>(null)
+  const minInitialValue = initialValues.values[0]
+  const maxInitialValue = initialValues.values[initialValues.values.length - 1]
+  const [currentValues, setCurrentValues] = useState<any>({ min: minInitialValue, max: maxInitialValue })
+  const [position, setPosition] = useState({ min: minInitialValue, max: maxInitialValue })
+  const [drag, setDrag] = useState<InputType | null>(null)
+  const [dragging, setDragging] = useState<boolean>(false)
   const sliderRef = useRef<HTMLDivElement>(null)
   const minBulletRef = useRef<HTMLDivElement>(null)
   const maxBulletRef = useRef<HTMLDivElement>(null)
   const minInputRef = useRef<HTMLInputElement>(null)
   const maxInputRef = useRef<HTMLInputElement>(null)
 
-  const onChange = (values: any): void => {
-    position.max = values.max
-    position.min = values.min
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>, element: InputType) => {
+    const newMin: number = element === InputType.MIN ? parseInt(e.target.value) : currentValues.min
+    const newMax: number = element === InputType.MAX ? parseInt(e.target.value) : currentValues.max
+
+    setCurrentValues({
+      min: getNewMinValue(newMin, minInitialValue, currentValues.max),
+      max: getNewMaxValue(newMax, maxInitialValue, currentValues.min),
+    })
+    setPosition({
+      min: getNewMinValue(newMin, minInitialValue, currentValues.max),
+      max: getNewMaxValue(newMax, maxInitialValue, currentValues.min),
+    })
   }
-
-  useEffect(() => {
-    onChange({ min: minValue, max: maxValue })
-    window.addEventListener('mouseup', handleMouseUp)
-    window.addEventListener('touchend', handleMouseUp)
-    return () => {
-      window.removeEventListener('mouseup', handleMouseUp)
-      window.addEventListener('touchend', handleMouseUp)
-    }
-  }, [])
-
-  useEffect(() => {
-    if (!isDragging) {
-      if (dragType) {
-        updateInput(values[dragType], dragType)
-      }
-      onChange(values)
-      setDragType(null)
-    }
-  }, [isDragging])
 
   const parseValues = (value: any): void => {
     let newValues: any
     setPosition(value)
 
     if (rangeType === RangeType.FIXED) {
-      if (!initialValues.values) return
-      newValues = setBulletsToStep(value, initialValues.values)
+      newValues = chooseNextStep(value, initialValues.values)
     } else {
       newValues = { min: Math.round(value.min), max: Math.round(value.max) }
     }
 
-    if (newValues.min !== values.min || newValues.max !== values.max) {
-      setValues(newValues)
+    if (newValues.min !== currentValues.min || newValues.max !== currentValues.max) {
+      setCurrentValues(newValues)
     }
-  }
-
-  const updateInput = (value: number, input: InputType.MIN | InputType.MAX) => {
-    if (isNaN(value)) {
-      escape()
-      return
-    }
-    switch (input) {
-      case InputType.MIN:
-        if (value < minValue) {
-          value = minValue
-        }
-        if (value >= values.max) {
-          value = values.max - 1
-        }
-        parseValues({ ...values, min: value })
-        onChange({ ...values, min: value })
-        break
-      case InputType.MAX:
-        if (value > maxValue) {
-          value = maxValue
-        }
-        if (value <= values.min) {
-          value = values.min + 1
-        }
-        parseValues({ ...values, max: value })
-        onChange({ ...values, max: value })
-        break
-    }
-    cleanTemps()
-  }
-
-  const cleanTemps = () => {
-    setTempValues({
-      min: null,
-      max: null,
-    })
-  }
-
-  const escape = () => {
-    setValues({
-      min: tempValues.min || values.min,
-      max: tempValues.max || values.max,
-    })
-    cleanTemps()
-    blurInputs()
-  }
-
-  const blurInputs = () => {
-    minInputRef.current!.blur()
-    maxInputRef.current!.blur()
-  }
-
-  const handleKeyboard = (event: React.KeyboardEvent<HTMLInputElement>, input: InputType.MIN | InputType.MAX) => {
-    switch (event.key) {
-      case 'Enter':
-        updateInput(input === InputType.MIN ? values.min : values.max, input)
-        break
-      case 'Escape':
-        escape()
-        break
-    }
-  }
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>, input: InputType) => {
-    const newMin: number = input === InputType.MIN ? parseInt(e.target.value) : values.min
-    const newMax: number = input === InputType.MAX ? parseInt(e.target.value) : values.max
-
-    setTempValues({
-      min: tempValues.min || values.min,
-      max: tempValues.max || values.max,
-    })
-    setValues({
-      min: getNewMinValue(newMin, minValue, values.max),
-      max: getNewMaxValue(newMax, maxValue, values.min),
-    })
-    setPosition({
-      min: getNewMinValue(newMin, minValue, values.max),
-      max: getNewMaxValue(newMax, maxValue, values.min),
-    })
   }
 
   const handleMouseMove = (event: any) => {
-    if (!isDragging) return
     let clientX: any
-    if (window.TouchEvent && (event.originalEvent instanceof TouchEvent || event.nativeEvent instanceof TouchEvent)) {
-      clientX = event.touches[0].clientX
-    } else {
-      clientX = event.clientX
-    }
-    const rangeLineRect = sliderRef.current!.getBoundingClientRect()
-    const position = (clientX - rangeLineRect.left) / rangeLineRect.width
+
+    if (!dragging) return
+
+    clientX = event.clientX
+    const sliderRect = sliderRef.current!.getBoundingClientRect()
+    const position = (clientX - sliderRect.left) / sliderRect.width
     if (position < 0 || position > 1) return
-    const newValue = minValue + position * (maxValue - minValue)
-    if (dragType === InputType.MIN && newValue < values.max - 1) {
-      parseValues({ ...values, min: newValue })
-    } else if (dragType === InputType.MAX && newValue > values.min + 1) {
-      parseValues({ ...values, max: newValue })
+    const newValue = minInitialValue + position * (maxInitialValue - minInitialValue)
+    if (drag === InputType.MIN && newValue < currentValues.max - 1) {
+      parseValues({ ...currentValues, min: newValue })
+    } else if (drag === InputType.MAX && newValue > currentValues.min + 1) {
+      parseValues({ ...currentValues, max: newValue })
     }
   }
 
-  const handleMouseUp = (event: Event) => {
-    event.preventDefault()
-    setIsDragging(false)
+  const handleMouseUp = () => {
+    setDragging(false)
   }
 
-  const handleMouseDown = (event: any, type: InputType.MIN | InputType.MAX) => {
-    event.preventDefault()
-    setIsDragging(true)
-    setDragType(type)
+  const handleMouseDown = (type: InputType) => {
+    setDragging(true)
+    setDrag(type)
   }
+
+  const changeBulletPosition = (values: any): void => {
+    position.max = values.max
+    position.min = values.min
+  }
+
+  const updateInput = (value: number, input: InputType) => {
+    switch (input) {
+      case InputType.MIN:
+        if (value < minInitialValue) {
+          value = minInitialValue
+        }
+        if (value >= currentValues.max) {
+          value = currentValues.max - 1
+        }
+        parseValues({ ...currentValues, min: value })
+        changeBulletPosition({ ...currentValues, min: value })
+        break
+      case InputType.MAX:
+        if (value > maxInitialValue) {
+          value = maxInitialValue
+        }
+        if (value <= currentValues.min) {
+          value = currentValues.min + 1
+        }
+        parseValues({ ...currentValues, max: value })
+        changeBulletPosition({ ...currentValues, max: value })
+        break
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('mouseup', handleMouseUp)
+  }, [])
+
+  useEffect(() => {
+    if (!dragging) {
+      if (drag) {
+        updateInput(currentValues[drag], drag)
+      }
+      changeBulletPosition(currentValues)
+      setDrag(null)
+    }
+  }, [])
 
   return (
     <div className="flex justify-between items-center my-6">
@@ -182,49 +129,42 @@ export default function RangeSlider({
         {rangeType === RangeType.NORMAL ? (
           <input
             type="number"
-            className="w-24 rounded-md border text-right border-gray-400 mr-4"
+            className="w-16 p-0.5 rounded-md border text-right border-gray-400 mr-5"
             ref={minInputRef}
-            value={values.min}
+            value={currentValues.min}
             onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleChange(e, InputType.MIN)}
-            onKeyDown={(event: React.KeyboardEvent<HTMLInputElement>) => handleKeyboard(event, InputType.MIN)}
-            onBlur={() => escape()}
           />
         ) : (
-          <span className="w-24 text-right mr-4">{values.min}</span>
+          <span className="w-24 text-right mr-4">{currentValues.min}</span>
         )}
         <div
           className="w-full h-12 relative before:block before:absolute before:-inset-x-0 before:h-1.5 before:mt-5 before:bg-gray-800"
           ref={sliderRef}
           onMouseMove={handleMouseMove}
-          onTouchMove={handleMouseMove}
         >
           <div
             className="w-6 h-6 absolute rounded-full mt-2.5 bg-gray-800 cursor-grab hover:w-8 hover:h-8 hover:mt-1.5 hover:cursor-grabbing"
             ref={minBulletRef}
-            onMouseDown={(e: React.MouseEvent) => handleMouseDown(e, InputType.MIN)}
-            onTouchStart={(e: React.TouchEvent) => handleMouseDown(e, InputType.MIN)}
-            style={{ left: setBulletPosition(position.min, minBulletRef, minValue, maxValue) }}
+            onMouseDown={() => handleMouseDown(InputType.MIN)}
+            style={{ left: setBulletPosition(position.min, minBulletRef, minInitialValue, maxInitialValue) }}
           ></div>
           <div
             className="w-6 h-6 absolute rounded-full mt-2.5 bg-gray-800 cursor-grab hover:w-8 hover:h-8 hover:mt-1.5 hover:cursor-grabbing"
             ref={maxBulletRef}
-            onMouseDown={(e: React.MouseEvent) => handleMouseDown(e, InputType.MAX)}
-            onTouchStart={(e: React.TouchEvent) => handleMouseDown(e, InputType.MAX)}
-            style={{ left: setBulletPosition(position.max, maxBulletRef, minValue, maxValue) }}
+            onMouseDown={() => handleMouseDown(InputType.MAX)}
+            style={{ left: setBulletPosition(position.max, maxBulletRef, minInitialValue, maxInitialValue) }}
           ></div>
         </div>
         {rangeType === RangeType.NORMAL ? (
           <input
             type="number"
-            className="w-24 rounded-md border border-gray-400 ml-6"
+            className="w-16 p-0.5 rounded-md border border-gray-400 ml-5"
             ref={maxInputRef}
-            value={values.max}
+            value={currentValues.max}
             onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleChange(e, InputType.MAX)}
-            onKeyDown={(event: React.KeyboardEvent<HTMLInputElement>) => handleKeyboard(event, InputType.MAX)}
-            onBlur={() => escape()}
           />
         ) : (
-          <span className="w-24 text-center ml-1.5">{values.max}</span>
+          <span className="w-24 text-center ml-1.5">{currentValues.max}</span>
         )}
       </div>
     </div>
